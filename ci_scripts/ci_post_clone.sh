bf8becc0527a5bd45750453950d07f4697020295
neon443 <69979447+neon443@users.noreply.github.com>
2025-06-07 19:23:40 +0100

    update CI script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,5 +6,9 @@
 #  Created by neon443 on 07/06/2025.
 #  
 
-cd SRCROOT
-pwd
+#working dir starts at /Volumes/workspace/repository/ci_scripts
+cd ..
+cd Frameworks
+wget -O frameworks.zip https://files.catbox.moe/8094yg.zip
+unzip frameworks.zip
+rm frameworks.zip
