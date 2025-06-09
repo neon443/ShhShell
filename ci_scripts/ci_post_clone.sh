c3c25e9d7d702f10fbb538bd6e1fe41b59d22877
neon443 <69979447+neon443@users.noreply.github.com>
2025-06-09 13:51:25 +0100

    change to a env var

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,7 +7,7 @@
 #  
 
 #working dir starts at /Volumes/workspace/repository/ci_scripts
-cd ..
+cd CI_PRIMARY_REPOSITORY_PATH
 mkdir Frameworks; cd Frameworks
 curl -o frameworks.zip https://files.catbox.moe/8094yg.zip
 unzip frameworks.zip
