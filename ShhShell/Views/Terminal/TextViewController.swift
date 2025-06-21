5fafc529130788b3c56872ac2e451cc9f237a111
neon443 <69979447+neon443@users.noreply.github.com>
2025-06-21 12:50:28 +0100

    added syntax highlighting - and a new runtime error :cry

--- ShhShell/Views/Terminal/TextViewController.swift
+++ ShhShell/Views/Terminal/TextViewController.swift
@@ -9,12 +9,15 @@ import Foundation
 import UIKit
 import SwiftUI
 import Runestone
+import TreeSitterBashRunestone
 
 struct TextViewController: UIViewRepresentable {
 	@Binding var text: String
 	
 	func makeUIView(context: Context) -> TextView {
+		let languageMode = TreeSitterLanguageMode(language: .bash)
 		let textView = TextView()
+		setTextViewState(on: textView)
 		textView.translatesAutoresizingMaskIntoConstraints = false
 		textView.backgroundColor = .systemBackground
 		return textView
@@ -22,5 +25,15 @@ struct TextViewController: UIViewRepresentable {
 	
 	func updateUIView(_ textView: TextView, context: Context) {
 		textView.text = text
+	}
+	
+	private func setTextViewState(on textView: TextView) {
+		DispatchQueue.global(qos: .userInitiated).async {
+			let text = self.text
+			let state = TextViewState(text: text, language: .bash)
+			DispatchQueue.main.async {
+				textView.setState(state)
+			}
+		}
 	}
 }
