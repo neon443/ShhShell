9cab6baea495546ac2a8f28c30d06dc77619cf7f
neon443 <69979447+neon443@users.noreply.github.com>
2025-08-27 18:51:41 +0100

    startedOnOnboarding

---
+++ ShhShell/Views/Onboarding/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  ShhShell
+//
+//  Created by neon443 on 27/08/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Welcome")
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
